d8374644ad0f173e2b953c9398209a0d1a851e78
Zain Fadhlurroman <zainf2206@gmail.com>
2025-12-19 00:51:21 +0700

    fix lagi deh

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -20,3 +20,32 @@ export const listSkills = [
   { name: "Cypress", icon: "icons/cypress.png" },
   { name: "MySQL", icon: "icons/mysql.png" },
 ];
+
+export const listIndustries = [
+  { name: "Facebook", icon: "icons/facebook.png" },
+  { name: "Google", icon: "icons/google.png" },
+  { name: "Tokopedia", icon: "icons/tokopedia.png" },
+  { name: "Amazon", icon: "icons/amazon.png" },
+];
+export const listProjects = [
+  {
+    title: "Darkblade Studio – Web Platform Development",
+    desc: "Developed a fully responsive and scalable web platform for a fantasy game studio.",
+    background: "images/bg-2.webp",
+    list_tech: [
+      { name: "JavaScript", icon: "icons/js.png" },
+      { name: "React", icon: "icons/react.svg" },
+      { name: "Tailwind", icon: "icons/tailwind.png" },
+    ],
+  },
+  {
+    title: "Personal Developer Portfolio",
+    desc: "Animated portfolio with modern frontend stack and performance optimization.",
+    background: "images/bg-3.webp",
+    list_tech: [
+      { name: "React", icon: "icons/react.svg" },
+      { name: "Tailwind", icon: "icons/tailwind.png" },
+      { name: "JavaScript", icon: "icons/js.png" },
+    ],
+  },
+];

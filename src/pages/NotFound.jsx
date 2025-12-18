6af6a70cc2b4afd4a38c0506bdccab37e6f8a0ed
Zain Fadhlurroman <zainf2206@gmail.com>
2025-12-19 00:06:25 +0700

    fix github pages routing

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
       <h2 className="font-barlow text-6xl font-bold">404</h2>
       <div className="mt-1 flex items-center gap-2 text-lg font-semibold">
         <i className="fa fa-chevron-right rotate-x-180"></i>
-        NOT FOUND
+        NOT FOUND BLOKKKK
       </div>
     </div>
   );
